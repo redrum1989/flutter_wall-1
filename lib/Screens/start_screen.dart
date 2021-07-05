a5c2226eb82d6696cb0eede0f213e47ad8e12a8d
Nyancake <rence.natera@gmail.com>
2021-07-05 22:21:26 +0800

    Added pickahu.jpg

--- lib/Screens/start_screen.dart
+++ lib/Screens/start_screen.dart
@@ -5,15 +5,16 @@ import 'package:flutter_wall/Screens/student_register_screen.dart';
 import 'package:flutter_wall/Services/AuthenticationService.dart';
 import 'package:provider/provider.dart';
 
+import 'initial_screen.dart';
+
 class StartScreen extends StatefulWidget {
-  const StartScreen({ Key key }) : super(key: key);
+  const StartScreen({Key key}) : super(key: key);
 
   @override
   _StartScreenState createState() => _StartScreenState();
 }
 
 class _StartScreenState extends State<StartScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,62 +32,65 @@ class _StartScreenState extends State<StartScreen> {
                   ),
                 ],
               ),
-
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
                     style: ButtonStyle(
-                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 80, vertical: 10)),
-                      backgroundColor: MaterialStateProperty.all(Color(0xfffe4f00)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 80, vertical: 10)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xfffe4f00)),
                       shape: MaterialStateProperty.all(
                         RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(22.0),
                         ),
                       ),
                     ),
-                    onPressed:(){
+                    onPressed: () {
                       Navigator.of(context).pushNamed(LoginScreen.routeName);
                     },
-                    child: Text("LOGIN",
-                    style: TextStyle(fontSize: 20)),
+                    child: Text("LOGIN", style: TextStyle(fontSize: 20)),
                   ),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 5)),
+                  Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 60, vertical: 5)),
                   ElevatedButton(
                     style: ButtonStyle(
-                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 60, vertical: 10)),
-                      backgroundColor: MaterialStateProperty.all(Color(0xff0b5c5d)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 60, vertical: 10)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff0b5c5d)),
                       shape: MaterialStateProperty.all(
                         RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(22.0),
                         ),
                       ),
                     ),
-                    onPressed:(){
+                    onPressed: () {
                       Navigator.of(context).pushNamed(ChooseScreen.routeName);
                     },
-                    child: Text("REGISTER",
-                    style: TextStyle(fontSize: 20)),
+                    child: Text("REGISTER", style: TextStyle(fontSize: 20)),
                   ),
                 ],
               ),
-              Padding(padding: EdgeInsets.symmetric(
-                    horizontal: 5, vertical: 30,
-                  )),
+              Padding(
+                  padding: EdgeInsets.symmetric(
+                horizontal: 5,
+                vertical: 30,
+              )),
               TextButton(
-                onPressed:(){
+                onPressed: () {
                   context.read<AuthenticationService>().signInAnon();
                 },
                 child: Text(
                   "Continue as Guest",
-                 style: TextStyle(
+                  style: TextStyle(
                       decoration: TextDecoration.underline,
                       fontSize: 15,
-                      color: Colors.black
-                    ),
+                      color: Colors.black),
                 ),
               ),
-             
             ],
           ),
         ),
