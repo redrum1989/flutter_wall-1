02c26e3b578f82894886dc74aa36bb6e92ff1dc1
redrum1989 <jpncultra@gmail.com>
2021-07-07 12:59:45 +0800

    app barappbar changes

--- lib/Screens/alumni_register_screen.dart
+++ lib/Screens/alumni_register_screen.dart
@@ -20,107 +20,105 @@ class _AlumniRegisterScreenState extends State<AlumniRegisterScreen> {
   TextEditingController _nameTextController = new TextEditingController();
   TextEditingController _collegeTextController = new TextEditingController();
   TextEditingController _yearStartedTextController = new TextEditingController();
-TextEditingController _yearGraduatedTextController = new TextEditingController();
+  TextEditingController _yearGraduatedTextController = new TextEditingController();
   TextEditingController _currentWorkTextController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
+      body: Stack(
+        children: [
 
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.red[100],
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red[100],
+            ),
+          ),
+
+          ListView(
+            children: [
+              SizedBox(height:50),
+              Center(child: Text("Email")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _emailTextController,
+                ),
               ),
-            ),
-
-            ListView(
-              children: [
-                SizedBox(height:50),
-                Center(child: Text("Email")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _emailTextController,
+              Center(child: Text("Password")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _pwTextController,
+                  obscureText: true,
+                ),
+              ),
+              Center(child: Text("College")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _collegeTextController,
+                ),
+              ),
+              Center(child: Text("Year Started")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _yearStartedTextController,
+                ),
+              ),
+              Center(child: Text("Year Graduated")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _yearGraduatedTextController,
+                ),
+              ),
+              Center(child: Text("Name")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _nameTextController,
+                ),
+              ),
+              Center(child: Text("CurrentWork")),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
+                child: TextFormField(
+                  controller: _currentWorkTextController,
+                ),
+              ),
+              SizedBox(height: 50),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed:() async {
+                      AlumniData user = new AlumniData(
+                        email: _emailTextController.text,
+                        password: _pwTextController.text,
+                        name: _nameTextController.text,
+                        college: _collegeTextController.text,
+                        yearStarted: _yearStartedTextController.text,
+                        yearGraduated: _yearGraduatedTextController.text,
+                        currentWork: _currentWorkTextController.text
+                      );
+                      await Database().registerAlumniData(context, user);
+                      Navigator.of(context).popUntil(ModalRoute.withName(InitialScreen.routeName));
+                    },
+                    child: Text("Register"),
                   ),
-                ),
-                Center(child: Text("Password")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _pwTextController,
-                    obscureText: true,
+                  ElevatedButton(
+                    onPressed:(){
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Back"),
                   ),
-                ),
-                Center(child: Text("College")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _collegeTextController,
-                  ),
-                ),
-                Center(child: Text("Year Started")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _yearStartedTextController,
-                  ),
-                ),
-                Center(child: Text("Year Graduated")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _yearGraduatedTextController,
-                  ),
-                ),
-                Center(child: Text("Name")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _nameTextController,
-                  ),
-                ),
-                Center(child: Text("CurrentWork")),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 30),
-                  child: TextFormField(
-                    controller: _currentWorkTextController,
-                  ),
-                ),
-                SizedBox(height: 50),
-                ButtonBar(
-                  alignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed:() async {
-                        AlumniData user = new AlumniData(
-                          email: _emailTextController.text,
-                          password: _pwTextController.text,
-                          name: _nameTextController.text,
-                          college: _collegeTextController.text,
-                          yearStarted: _yearStartedTextController.text,
-                          yearGraduated: _yearGraduatedTextController.text,
-                          currentWork: _currentWorkTextController.text
-                        );
-                        await Database().registerAlumniData(context, user);
-                        Navigator.of(context).popUntil(ModalRoute.withName(InitialScreen.routeName));
-                      },
-                      child: Text("Register"),
-                    ),
-                    ElevatedButton(
-                      onPressed:(){
-                        Navigator.of(context).pop();
-                      },
-                      child: Text("Back"),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/Screens/student_register_screen.dart
+++ lib/Screens/student_register_screen.dart
@@ -26,270 +26,271 @@ class _StudentRegisterScreenState extends State<StudentRegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Container(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
-                    child: Image(image: AssetImage('assets/img/student bg.jpg',))),
-                )
-              ],
-            ),
-            AppBar(
-              iconTheme: IconThemeData(
-              color: Colors.black),
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-            ),
-            ListView(
-              children: [
-                SizedBox(height: 250,),
-                Container(
-                  height: 30,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
-                    child: Text('WELCOME,',
-                    style: TextStyle(
-                      fontFamily: 'mont',
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
+                  child: Image(image: AssetImage('assets/img/student bg.jpg',))),
+              )
+            ],
+          ),
+          AppBar(
+                iconTheme: IconThemeData(
+                color: Colors.black),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+          ListView(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(height: 260,),
+                ],
+              ),
+              Container(
+                height: 30,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text('WELCOME,',
+                  style: TextStyle(
+                    fontFamily: 'mont',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 30,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
+                  child: Text('STUDENT',
+                  style: TextStyle(
+                    fontFamily: 'mont',
                       fontWeight: FontWeight.w900,
-                      fontSize: 30,
-                      color: Colors.white
-                    ),
-                    ),
-                  ),
+                      fontSize: 60,
+                  ),),
                 ),
-                Container(
-                  height: 60,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
-                    child: Text('STUDENT',
-                    style: TextStyle(
-                      fontFamily: 'mont',
-                        fontWeight: FontWeight.w900,
-                        fontSize: 60,
-                    ),),
-                  ),
-                ),
-                SizedBox(height:5),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("Email",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[200],
-                    fillColor: Colors.deepOrange[600],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[600],
-                      width: 2
-                    )
+              ),
+              SizedBox(height:5),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("Email",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[200],
+                  fillColor: Colors.deepOrange[600],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[600],
+                    width: 2
                   )
-                
-                    ),
-                    controller: _emailTextController,
+                )
+              
                   ),
+                  controller: _emailTextController,
                 ),
-
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("Password",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[200],
-                    fillColor: Colors.deepOrange[300],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[300],
-                      width: 2
-                    )
-                  )  
-                    ),
-                    controller: _pwTextController,
-                    obscureText: true,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("Password",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[200],
+                  fillColor: Colors.deepOrange[300],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[300],
+                    width: 2
+                  )
+                )  
                   ),
+                  controller: _pwTextController,
+                  obscureText: true,
                 ),
-
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("College",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[200],
-                    fillColor: Colors.deepOrange[600],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[600],
-                      width: 2
-                    )
-                  )  
-                ),
-                    controller: _collegeTextController,
               ),
-            ),
-
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("Current Year",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[200],
-                    fillColor: Colors.deepOrange[300],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[300],
-                      width: 2
-                    )
-                  )  
-                ),
-                    controller: _currentYearTextController,
+      
+      
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("College",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[200],
+                  fillColor: Colors.deepOrange[600],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[600],
+                    width: 2
+                  )
+                )  
               ),
+                  controller: _collegeTextController,
             ),
-
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("Name",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[800],
-                    fillColor: Colors.deepOrange[600],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[600],
-                      width: 2
-                    )
-                  )  
-                ),
-                    controller: _nameTextController,
+          ),
+      
+      
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("Current Year",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[200],
+                  fillColor: Colors.deepOrange[300],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[300],
+                    width: 2
+                  )
+                )  
               ),
+                  controller: _currentYearTextController,
             ),
-
-            
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                    child: Text("Gender",
-                    style: TextStyle(
-                    fontFamily: 'mont',
-                    fontWeight: FontWeight.w900
-                ),),
-                  )),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    filled: true,
-                    focusColor: Colors.grey[200],
-                    fillColor: Colors.deepOrange[300],
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: Colors.deepOrange[300],
-                      width: 2
-                    )
-                  )  
-                ),
-                    controller: _genderTextController,
+          ),
+      
+      
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("Name",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[800],
+                  fillColor: Colors.deepOrange[600],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[600],
+                    width: 2
+                  )
+                )  
               ),
+                  controller: _nameTextController,
             ),
-                SizedBox(height: 10),
-                ButtonBar(
-                  alignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Color(0xff0b5c5d)),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(20))
-                            )
+          ),
+      
+          
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Text("Gender",
+                  style: TextStyle(
+                  fontFamily: 'mont',
+                  fontWeight: FontWeight.w900
+              ),),
+                )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                  filled: true,
+                  focusColor: Colors.grey[200],
+                  fillColor: Colors.deepOrange[300],
+                  enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Colors.deepOrange[300],
+                    width: 2
+                  )
+                )  
+              ),
+                  controller: _genderTextController,
+            ),
+          ),
+              SizedBox(height: 10),
+              ButtonBar(
+                alignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Color(0xff0b5c5d)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20))
                           )
-                        ),
-                        onPressed:() async {
-                          StudentData user = new StudentData(
-                            email: _emailTextController.text,
-                            password: _pwTextController.text,
-                            college: _collegeTextController.text,
-                            currentYear: _currentYearTextController.text,
-                            name: _nameTextController.text,
-                          );
-                          await Database().registerStudentData(context, user);
-                          Navigator.of(context).popUntil(ModalRoute.withName(InitialScreen.routeName));
-                        },
-                        child: Text("REGISTER",
-                        ),
+                        )
+                      ),
+                      onPressed:() async {
+                        StudentData user = new StudentData(
+                          email: _emailTextController.text,
+                          password: _pwTextController.text,
+                          college: _collegeTextController.text,
+                          currentYear: _currentYearTextController.text,
+                          name: _nameTextController.text,
+                        );
+                        await Database().registerStudentData(context, user);
+                        Navigator.of(context).popUntil(ModalRoute.withName(InitialScreen.routeName));
+                      },
+                      child: Text("REGISTER",
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
