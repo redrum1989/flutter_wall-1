5ba540facb9e2634d3a11327b92f4202bf87ee34
redrum1989 <jpncultra@gmail.com>
2021-07-11 21:46:47 +0800

    commit jp

--- lib/Model/Post.dart
+++ lib/Model/Post.dart
@@ -2,18 +2,21 @@ class Post {
   String title;
   String text;
   String authorUID;
+  String college;
 
   Post({
     this.title,
     this.text,
-    this.authorUID
+    this.authorUID,
+    this.college
   });
 
   Map<String, dynamic> returnPostData(){
     return {
       "title" : this.title,
       "text" : this.text,
-      "authorUID" : this.authorUID
+      "authorUID" : this.authorUID,
+      "college" : this.college
     };
   }
 }
